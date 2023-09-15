c163a63d41cad649454feff80fd2e73c322e8b4c
Jaakko252525 <124563002+Jaakko252525@users.noreply.github.com>
2023-09-15 18:26:59 +0300

    initial commit

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+
+
+
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path("polls/", include("polls.urls")),
+    path("admin/", admin.site.urls),
+]
+
+
+
