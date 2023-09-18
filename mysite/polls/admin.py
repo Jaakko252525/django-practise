60c80cab8f92f041d3939eb02fdba7b4e53f8a9c
Jaakko252525 <124563002+Jaakko252525@users.noreply.github.com>
2023-09-18 16:28:47 +0300

    changes

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,15 +1,28 @@
 
+
 from django.contrib import admin
 
-from .models import Question
-
-admin.site.register(Question)
-
-# Register your models here.
+from .models import Choice, Question
 
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
 
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+        ("siuu", {"fields": ["siuu"]}),
+        ("hmmmm", {"fields": ["testi"]}),
+        ("greating", {"fields": ["greating"]})
+
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
 
 
 
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -13,6 +13,23 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
+    siuu = models.CharField(max_length=200)
+    testi = models.CharField(max_length=200)
+
+    MOI = "moi_1"
+    HEY = "hey_1"
+    HI = "hi_1"
+    # choices 
+    choices = [(MOI, "moi"),( HEY,"hey"), (HI,"hi")]
+
+    greating = models.CharField(
+        max_length=5,
+        choices=choices,
+        default="moi",
+    )
+
+
+
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
